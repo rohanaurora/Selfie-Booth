c48a006822b7c5d1fb2220bcbe9cafcbeb4d90bb
Rohan Aurora <roaurora@ebay.com>
2014-10-17 00:01:18 -0700

    Remove Attempting to badge the application icon log.

--- SelfieBooth/SelfieBooth/AppDelegate.m
+++ SelfieBooth/SelfieBooth/AppDelegate.m
@@ -56,8 +56,6 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
-    
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
     NSString * const ServerHost = @"www.google.com";
